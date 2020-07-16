1969f10311eb369226071b9e87ba82aa6ba58eec
sami-chaaban <36083637+sami-chaaban@users.noreply.github.com>
2020-07-16 17:49:15 +0100

    Update plotakta.py
    Bug fix

--- plotakta.py
+++ plotakta.py
@@ -1,5 +1,5 @@
 #SAMI CHAABAN
-#VERSION 1.1 2020-02-25
+#VERSION 1.2 2020-07-16
 
 import optparse
 import numpy as np
@@ -128,7 +128,7 @@ def mainloop(params):
                 'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
                 'E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12',
                 'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12',
-                'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12'
+                'G1','G2','G3','G4','G5','G6','G7','G8','G9','G10','G11','G12',
                 'H1','H2','H3','H4','H5','H6','H7','H8','H9','H10','H11','H12']
 
 
@@ -165,6 +165,9 @@ def mainloop(params):
 
                 dig1lab_frac.append(plate[p])
                 p += 1
+
+                if p == 95:
+                    break
 
                 alreadydone = 1
 
